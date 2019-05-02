682b64d5c7b63da0df3372b4a3a25f1e914de733
rztprog <rztprog@outlook.fr>
2019-05-02 14:46:10 +0200

    Change to iPhone-like calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,39 +5,39 @@ class Calculator extends React.Component {
 	render() {
 		return (
 			<div className="Calculator">
-			<table>
+			<table cellspacing="1">
 			<tbody>
 			<tr className="Head">
 			<th colSpan="4">0</th>
 			</tr>
-			<tr>
+			<tr className="Grey">
 			<td>AC</td>
 			<td>+/-</td>
 			<td>%</td>
-			<td>/</td>
+			<td className="Orange">/</td>
 			</tr>
-			<tr>
+			<tr className="LightGrey">
 			<td>7</td>
 			<td>8</td>
 			<td>9</td>
-			<td>*</td>
+			<td className="Orange">*</td>
 			</tr>
-			<tr>
+			<tr className="LightGrey">
 			<td>4</td>
 			<td>5</td>
 			<td>6</td>
-			<td>+</td>
+			<td className="Orange">+</td>
 			</tr>
-			<tr>
+			<tr className="LightGrey">
 			<td>1</td>
 			<td>2</td>
 			<td>3</td>
-			<td>-</td>
+			<td className="Orange">-</td>
 			</tr>
-			<tr>
+			<tr className="LightGrey">
 			<td colSpan="2">0</td>
 			<td>,</td>
-			<td>=</td>
+			<td className="Orange">=</td>
 			</tr>
 			</tbody>
 			</table>

898ad0117b33c4998eaa3e0cf09486046f85a5dc
rztprog <rztprog@outlook.fr>
2019-05-05 21:25:29 +0200

    Updates

---
+++ precache-manifest.e22d498af3e3cd2c7b2b0aa38eff4555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2447489ac57ee559390898775e75a11e",
+    "url": "/delivery06_calculator/index.html"
+  },
+  {
+    "revision": "b20311017f0e31901fb3",
+    "url": "/delivery06_calculator/static/css/main.d532416b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/delivery06_calculator/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "b20311017f0e31901fb3",
+    "url": "/delivery06_calculator/static/js/main.f61edafb.chunk.js"
+  },
+  {
+    "revision": "ce8896e910b590dc66d0",
+    "url": "/delivery06_calculator/static/js/runtime~main.bb16f194.js"
+  }
+]);
